924d1d2e18a03bdacf74325b616a35e564ed39e2
Victor Tavares <victor.tavares@mc1.com.br>
2017-11-03 16:53:24 -0200

    Criando estrutura inicial do projeto com cocoapods / Adicionando .gitignore

---
+++ Pods/Target Support Files/Pods-TopJavaRepos/Pods-TopJavaRepos-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TopJavaRepos : NSObject
+@end
+@implementation PodsDummy_Pods_TopJavaRepos
+@end
---
+++ Pods/Target Support Files/Pods-TopJavaReposTests/Pods-TopJavaReposTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TopJavaReposTests : NSObject
+@end
+@implementation PodsDummy_Pods_TopJavaReposTests
+@end
